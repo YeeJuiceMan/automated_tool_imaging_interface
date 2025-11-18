fa0aa631bc3d8899c4ba42074e94a41eb68879b4
cmiddaugh <107422116+cmiddaugh@users.noreply.github.com>
2025-11-18 10:06:25 -0500

    Update Initial Python Code.py

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -95,7 +95,7 @@ class StepperController:
 
     def rotate_degrees(self, degrees, clockwise=True):
         # calculate steps needed to rotate by a specific angle in degrees
-        steps = int(degrees * self.steps_per_rev )
+        steps = int((degrees/360) * self.steps_per_rev )
         direction = 1 if clockwise else -1
         sequence = self.step_sequence if clockwise else self.step_sequence[::-1]
         for _ in range(steps):
@@ -113,7 +113,7 @@ class ActuatorController:
         self.step_sequence = step_sequence
         self.steps_per_rev = steps_per_rev
         self.gear_ratio = gear_ratio
-        self.step_delay = 0.02
+        self.step_delay = 0.005
         self.current_step = 0
 
     def move(self, degrees, upward=True):
