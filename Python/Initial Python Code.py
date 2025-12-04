a445e99cb050f041c90417034f7b8be714ce3de8
cmiddaugh <107422116+cmiddaugh@users.noreply.github.com>
2025-12-04 13:31:43 -0500

    Update Initial Python Code.py

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -89,6 +89,8 @@ def setup_gpio():
                 VERT_STP2_BLACK, VERT_STP2_GREEN, VERT_STP2_RED, VERT_STP2_BLUE, 
                   STP_IN1, STP_IN2, STP_IN3, STP_IN4], GPIO.LOW)
 
+
+
 class StepperController:
     def __init__(self, step_pins, step_sequence, steps_per_rev, gear_ratio):
         self.step_pins = step_pins
@@ -109,6 +111,19 @@ class StepperController:
                     GPIO.output(self.step_pins[pin], step[pin])
                 time.sleep(self.step_delay)
 
+class Clock:
+    
+    def __init__(self, fps):
+        self.start = time.perf_counter()
+        self.frame_length = 1/fps
+    @property
+    def tick(self):
+        return int((time.perf_counter() - self.start)/self.frame_length)
+
+    def sleep(self):
+        r = self.tick + 1
+        while self.tick < r:
+            time.sleep(1/1000)
 
 class ActuatorController:
     def __init__(self, stepper1_pins, stepper2_pins, step_sequence, steps_per_rev, gear_ratio):
@@ -118,7 +133,8 @@ class ActuatorController:
         self.step_sequence = step_sequence
         self.steps_per_rev = steps_per_rev
         self.gear_ratio = gear_ratio
-        self.step_delay = 0.02
+        self.step_delay = 0.025
+        self.clock = Clock(100)
         self.current_step = 0
 
     def move(self, degrees, upward=True):
@@ -133,7 +149,9 @@ class ActuatorController:
                     GPIO.output(self.stepper1_pins[pin], step[pin])
                     GPIO.output(self.stepper2_pins[pin], step[pin])
                     print(self.stepper1_pins[pin], self.stepper2_pins[pin], step[pin])
-                time.sleep(self.step_delay)
+                #time.sleep(self.step_delay)
+                self.clock.sleep()
+                
             step_count += 1
 
             '''if step_count % 10 == 0:
