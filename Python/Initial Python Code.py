4c9153b896ec1b5771630caa7497003aeef89f4b
ey <ey@le-Mac.local>
2025-12-07 20:58:27 -0500

    python delay change

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -96,7 +96,7 @@ class StepperController:
         self.step_sequence = step_sequence
         self.steps_per_rev = steps_per_rev
         self.gear_ratio = gear_ratio
-        self.step_delay = 0.005
+        self.step_delay = 0.001
         self.current_step = 0
 
     def rotate_degrees(self, degrees, clockwise=True):
@@ -118,7 +118,7 @@ class ActuatorController:
         self.step_sequence = step_sequence
         self.steps_per_rev = steps_per_rev
         self.gear_ratio = gear_ratio
-        self.step_delay = 0.001
+        self.step_delay = 0.01
       
         self.current_step = 0
 
@@ -267,6 +267,7 @@ def automated_capture_sequence(tool_number, flute_number, layer_number, cameras,
         # 20 positions * 20 seconds = 400 seconds which would be 6.67 minutes
 
         all_file_paths = []
+        actuator.retract()
         actuator.extend()
         # initial positioning by starting with tool fully down
         #actuator.retract(360)
