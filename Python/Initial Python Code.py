3fde314e4193ade69e0cd46ccb0226cde20c88f2
ey <ey@le-Mac.local>
2025-12-07 20:57:11 -0500

    python delay change

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -96,7 +96,7 @@ class StepperController:
         self.step_sequence = step_sequence
         self.steps_per_rev = steps_per_rev
         self.gear_ratio = gear_ratio
-        self.step_delay = 0.01
+        self.step_delay = 0.005
         self.current_step = 0
 
     def rotate_degrees(self, degrees, clockwise=True):
