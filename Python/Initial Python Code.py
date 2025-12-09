760b98e534649ad80a10522c34f07b12f2edeec7
cmiddaugh <107422116+cmiddaugh@users.noreply.github.com>
2025-12-09 09:11:48 -0500

    test

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -258,7 +258,7 @@ def automated_capture_sequence(tool_number, flute_number, layer_number, cameras,
 
 
         all_file_paths = []
-        actuator.extend(400)
+        #actuator.extend(400)
         '''for _ in range (50):
             actuator.retract(400)
             time.sleep(3)
@@ -303,7 +303,7 @@ def automated_capture_sequence(tool_number, flute_number, layer_number, cameras,
                 time.sleep(0.3)
 
         #print(f"\nCapture sequence completed. Total images: {len(all_file_paths)}")
-        actuator.retract(400)
+        #actuator.retract(400)
         return all_file_paths
 
     except Exception as e:
