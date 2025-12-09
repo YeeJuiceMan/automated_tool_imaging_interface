69ad60501788a190f8393cc1d21b30ec22588581
cmiddaugh <107422116+cmiddaugh@users.noreply.github.com>
2025-12-09 09:42:30 -0500

    Update Initial Python Code.py

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -258,7 +258,7 @@ def automated_capture_sequence(tool_number, flute_number, layer_number, cameras,
 
 
         all_file_paths = []
-        actuator.extend(90)
+        actuator.extend(80)
         #actuator.extend(400)
         '''for _ in range (50):
             actuator.retract(400)
