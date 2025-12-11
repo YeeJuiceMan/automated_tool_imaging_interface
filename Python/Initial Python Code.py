f64c9b3151a51d698f0a53bd8b9123d879acfa8b
cmiddaugh <107422116+cmiddaugh@users.noreply.github.com>
2025-12-11 11:14:27 -0500

    Update Initial Python Code.py

--- Python/Initial Python Code.py
+++ Python/Initial Python Code.py
@@ -275,7 +275,7 @@ def automated_capture_sequence(tool_number, flute_number, layer_number, cameras,
         time.sleep(0.5)
         #tracks height of camera needed to revert to
         cam_height = 0
-        cam_height += actuator.extend(920)
+        cam_height += actuator.extend(900)
        
         # initial positioning by starting with tool fully down
         image_paths = cameras.capture_images(tool_number, flute_number, layer_number, 0, 0, 0)
